c0a33b9e16893f11d8dcf6199d73214d6520b41c
elit <elityaev@yandex.ru>
2022-12-26 14:29:34 +0300

    add create new array method, input-output inf

--- Program.cs
+++ Program.cs
@@ -20,4 +20,24 @@ int GetLengthNewArray(string[] arr)
     return count;
 }
 
+string[] CreateNewArray( string[] arr, int size)
+{
+    int index = 0;
+    string[] newArr = new string[size];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newArr[index] = arr[i];  
+            index += 1;
+        }
+    }
+    return newArr;
+}
 
+Console.WriteLine("Задайте размер массива: ");
+int size = Convert.ToInt32(Console.ReadLine());
+string[] array = FillArray(size);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", array)}]");
+int arr_size = GetLengthNewArray(array);
+Console.WriteLine($"Новый массив: [{string.Join(", ", CreateNewArray(array, arr_size))}]");

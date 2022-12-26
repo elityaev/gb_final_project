fd9e2235a33708f4d6f7a93d3a5f32282630b054
elit <elityaev@yandex.ru>
2022-12-26 14:25:43 +0300

    add fill array method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] FillArray(int size)
+{
+    string[] arr = new string[size];
+    
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.WriteLine($"Введите {i+1} строку из {size} строк");
+        arr[i] = Console.ReadLine();
+    }
+    return arr;
+}
+
+
